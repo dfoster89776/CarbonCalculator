5db1cdb778e8b24843ade87ff4e486cbc3d559bc
David Foster <dfoster89776@icloud.com>
2014-02-05 12:06:10 +0000

    Further UI updates

--- dashboard.php
+++ dashboard.php
@@ -12,6 +12,8 @@
 		<title> Carbon Calculator Dashboard</title>
 		<?php 	require_once("files/standard/standard_includes.php"); ?>
 		<script src="files/dashboard/dashboard.js"></script>
+		<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+
 	</head>
 	
 	<body onload="initialise()">
@@ -21,20 +23,34 @@
 		<div class="container">
 		
 			<div class="row">
-			
-				<div class="col-md-8" id="activity_container">
-					
-					<?php include_once("files/dashboard/activity.php"); ?>
-									
-				</div>
 				
-				<div class="col-md-3 col-md-offset-1">
-					<p><button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#addJourney" style="width: 100%;">
+				<div class="col-md-3 col-md-push-9">
+					<div>
+					
+						<h2> Carbon Total </h2>
+						
+						<h4> This Month: </h4>
+						<div id="carbonTotalThisMonth">...</div>
+						<h4> Last Month: </h4>
+						<div id="carbonTotalLastMonth">...</div>
+					</div>
+					<br/>
+					<p><button class="btn btn-primary btn-lg" style="width: 100%;" onclick="openJourneyModal()">
 					  Add Journey
 					</button></p>
 					<p><button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#addMeterReading" style="width: 100%;">
 					  Add Meter Reading
 					</button></p>
+				</div>
+				
+				<div class="col-md-8 col-md-pull-3">
+					<div>
+					<?php include_once("files/dashboard/dashboardGraph.php"); ?>
+					</div>
+					<div id="activity_container">
+					
+					<?php include_once("files/dashboard/activity.php"); ?>
+					</div>
 				</div>
 			
 			</div>
@@ -44,19 +60,8 @@
 	<!-- JOURNEY MODAL -->	
 		<div class="modal fade" id="addJourney" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 		  <div class="modal-dialog">
-		    <div class="modal-content">
-		      <div class="modal-header">
-		        <button type="button" class="close" data-dismiss="modal" onclick="resetJourneyModal()" aria-hidden="true">&times;</button>
-		        <h4 class="modal-title" id="myModalLabel">Add Journey</h4>
-		      </div>
-		      <div class="modal-body" id="journey_body">
-		      	<?php include_once("files/dashboard/journey_modal.php"); ?>
-		      </div>
-		      <div class="modal-footer">
-		        <button type="button" class="btn btn-default" data-dismiss="modal" onclick="resetJourneyModal()">Close</button>
-		        <button type="button" class="btn btn-primary" onclick="submitJourney()" id="journey_submit_button">Submit</button>
-		      </div>
-		    </div><!-- /.modal-content -->
+		    <div class="modal-content" id="addJourneyModal">
+		   </div><!-- /.modal-content -->
 		  </div><!-- /.modal-dialog -->
 		</div><!-- /.modal -->	
 		
